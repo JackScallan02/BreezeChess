0c09982d7a1d0e7a112406b090dfd369445a577b
Jack <jack.gregory.scallan@gmail.com>
2025-01-04 20:14:08 -0500

    Made basic header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react';
+import MainToolBar from './components/MainToolBar';
 
-function App() {
+const App = () => {
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="w-full h-full absolute bg-gradient-to-r from-sky-400 to-blue-400">
+        <MainToolBar />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/MainToolBar.js
@@ -0,0 +1,35 @@
+import { React, useState } from 'react';
+import tailwindcsslogo from '../assets/tailwindcsslogo.png';
+
+const MainToolBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <header className="flex justify-between items-center text-black py-6 px-8 md:px-32 bg-white drop-shadow-md">
+          <a href="#">
+            <img src={tailwindcsslogo} alt="" className="w-52 hover:scale-105 transition-all" />
+          </a>
+          <ul className="xl:flex hidden items-center gap-12 font-semibold text-base">
+            <li className="p-3 hover:text-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Home</li>
+            <li className="p-3 hover:text-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Idk yet</li>
+            <li className="p-3 hover:text-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Idk yet</li>
+            <li className="p-3 hover:text-sky-400 hover:text-white rounded-md transition-all cursor-pointer">Contact</li>
+          </ul>
+          <button className='space-y-1 group xl:hidden' onClick={() => setMenuOpen(!menuOpen)}>
+            <div className='w-6 h-1 bg-black'></div>
+            <div className='w-6 h-1 bg-black'></div>
+            <div className='w-6 h-1 bg-black'></div>
+          </button>
+          <div className={`absolute xl:hidden top-20 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transform
+          ${menuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'} transition-opacity transition-transform duration-300 ease-in-out`}
+          >
+            <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">Home</li>
+            <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">Idk yet</li>
+            <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">Idk yet</li>
+            <li className="list-none w-full text-center p-4 hover:bg-sky-400 hover:text-white transition-all cursor-pointer">Contact</li>
+          </div>
+        </header>
+    )
+};
+
+export default MainToolBar;
