adf4f32acd1eba941c7cef3b13d8ca83f459eed0
Jack <jack.gregory.scallan@gmail.com>
2025-03-20 22:36:43 -0400

    installed prime react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./constants/router/router";
 import { AuthProvider } from "./contexts/AuthContext";
+import { PrimeReactProvider } from 'primereact/api';
 import { React } from 'react';
 
 export default function App() {
 
   return (
     <AuthProvider>
-      <div className="App min-h-screen bg-gradient-to-r from-sky-100 to-sky-200 dark:from-gray-800 dark:to-gray-900 dark:text-white">
-        <RouterProvider router={router} />
-      </div>
+      <PrimeReactProvider>
+        <div className="App min-h-screen bg-gradient-to-r from-sky-100 to-sky-200 dark:from-gray-800 dark:to-gray-900 dark:text-white">
+          <RouterProvider router={router} />
+        </div>
+      </PrimeReactProvider>
     </AuthProvider>
   );
 }
